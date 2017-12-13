638e46f00cd0c5ad782eedc1050f4d4b297c088c
Steve Bishop <xipooo@gmail.com>
2017-12-13 15:12:22 -0800

    Default param value demo

--- Examples/Front End Foundations/scripts/index.js
+++ Examples/Front End Foundations/scripts/index.js
@@ -1,24 +1,29 @@
-function addTwoNumbers(value1, value2) {
-    if (typeof value1 == "number" && typeof value2 == "number") {
-        return value1 + value2;
-    }
-    else {
-        return "Invalid entry";
-    }
-}
-console.log(addTwoNumbers(8, 9));
+// function addTwoNumbers(value1, value2) {
+//     if (typeof value1 == "number" && typeof value2 == "number") {
+//         return value1 + value2;
+//     }
+//     else {
+//         return "Invalid entry";
+//     }
+// }
+// console.log(addTwoNumbers(8, 9));
 
-if (true || false){
-    return console.log("true");
-}
-else {
-    return console.log("false");
-}
+// if (true || false){
+//     return console.log("true");
+// }
+// else {
+//     return console.log("false");
+// }
 
-let myString = "Hello";
-let myOtherString = myString;
+// let myString = "Hello";
+// let myOtherString = myString;
 
-myOtherString = "World";
+// myOtherString = "World";
 
-console.log(typeof myString);
-console.log(typeof myOtherString);
+// console.log(typeof myString);
+// console.log(typeof myOtherString);
+
+const defParam = (a, b = 20) => a * b;
+
+console.log(defParam(3, 5));
+console.log(defParam(6));
