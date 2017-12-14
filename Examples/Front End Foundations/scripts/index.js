b3ca944b28ca1cffaca7f071498786e14d324dc9
Steve Bishop <xipooo@gmail.com>
2017-12-14 07:54:13 -0800

    Scope example

--- Examples/Front End Foundations/scripts/index.js
+++ Examples/Front End Foundations/scripts/index.js
@@ -1,29 +1,16 @@
-// function addTwoNumbers(value1, value2) {
-//     if (typeof value1 == "number" && typeof value2 == "number") {
-//         return value1 + value2;
-//     }
-//     else {
-//         return "Invalid entry";
-//     }
-// }
-// console.log(addTwoNumbers(8, 9));
+var variable = "Global Scope";
 
-// if (true || false){
-//     return console.log("true");
-// }
-// else {
-//     return console.log("false");
-// }
+function myFunction(variable) {
+    //let variable = "Function Scope";
+    if (variable == "Global Scope") {
+        var variable = "Local Scope";
+        console.log(variable);
+    }
+    else {
+        console.log(variable);
+    }
+    console.log(variable);
+}
 
-// let myString = "Hello";
-// let myOtherString = myString;
-
-// myOtherString = "World";
-
-// console.log(typeof myString);
-// console.log(typeof myOtherString);
-
-const defParam = (a, b = 20) => a * b;
-
-console.log(defParam(3, 5));
-console.log(defParam(6));
+myFunction(variable);
+console.log(variable);
