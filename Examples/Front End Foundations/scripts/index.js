cbb19c5c1b2b2c2d6f6d2053d13163d9b88cd72d
Steve Bishop <xipooo@gmail.com>
2017-12-14 15:49:11 -0800

    Creating eleents

--- Examples/Front End Foundations/scripts/index.js
+++ Examples/Front End Foundations/scripts/index.js
@@ -1,5 +1,32 @@
-const renderName = () => {
-    let textEnteredElement = document.getElementById("textEntered");
-    textEnteredElement.innerHTML += document.getElementById("name").value;
-    console.log(document.getElementById("name").value);
-}
+(function () {
+    const $c = (element) => document.createElement(element);
+    const $t = (tag) => document.getElementsByTagName(tag);
+    const $id = (id) => document.getElementById(id);
+
+    let container = $c("div");
+    container.id = "container";
+
+    let body = $t("body");
+    body[0].appendChild(container);
+
+    let head = $t("head");
+    let title = $c("title");
+
+    title.innerHTML = "This is a document";
+    head[0].appendChild(title);
+
+    const addChild = () => {
+        let newElem = $c("div");
+        newElem.innerHTML = "This is a new Element created by append.";
+
+        let container = $id("container");
+        container.appendChild(newElem);
+        console.log("addChild was called");
+    }
+
+    let button = $c("button");
+    button.style = "color: red; background-color: black;";
+    button.innerHTML = "Click Me!";
+    button.onclick = addChild;
+    container.appendChild(button);
+})();
