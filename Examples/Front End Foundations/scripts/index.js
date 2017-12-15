c7308a18c133e73ecba3ba5cf648a9ec28b593bf
Steve Bishop <xipooo@gmail.com>
2017-12-15 15:46:06 -0800

    Person class  metnod example

--- Examples/Front End Foundations/scripts/index.js
+++ Examples/Front End Foundations/scripts/index.js
@@ -1,10 +1,20 @@
-class Vehicle {
-    constructor(make, model, year) {
-        this.make = make;
-        this.model = model;
-        this.year = year;
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+
+    run() {
+        console.log(this.name + " is running.");
+    }
+    walk() {
+        console.log(this.name + " is walking.");
+    }
+    greeting() {
+        console.log("Hello, my name is " + this.name + ".");
     }
 }
 
-let car = new Vehicle("Nissan", "Rogue", 2017);
-console.log(car);
+let Andrew = new Person("Andrew");
+Andrew.run();
+Andrew.walk();
+Andrew.greeting();
