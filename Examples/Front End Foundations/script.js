6a3173013d3f5845a748a596ee6e096970c4a5d5
Steve Bishop <xipooo@gmail.com>
2017-12-18 15:15:00 -0800

    JSON demo

--- Examples/Front End Foundations/script.js
+++ Examples/Front End Foundations/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-    $("#driver").click(
-        function () {
-            $("#stage").load("info.html");
-        });
+    let newRequest = new XMLHttpRequest();
+    newRequest.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200){
+            let myObj = JSON.parse(this.responseText);
+            document.getElementById("example").innerHTML = myObj.name;
+        }
+    }
+    newRequest.open("GET", "https://api.github.com/users/IgnacioLuciano", true);
+    newRequest.send();
 });
