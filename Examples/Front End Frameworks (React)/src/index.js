9680b6349bf65698014a03376bac835c60f71781
Steve Bishop <xipooo@gmail.com>
2017-12-20 13:00:36 -0800

    JSX Demo

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,31 +1,10 @@
 import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
 
-const price1 = React.createElement('li', null, 'Price 1');
-const price2 = React.createElement('li', null, 'Price 2');
-
 ReactDOM.render(
-    React.createElement(
-        'ul',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item 1',
-            React.createElement(
-                'ul',
-                null,
-                price1)),
-        React.createElement(
-            'li',
-            null,
-            'Item 2',
-            React.createElement(
-                'ul',
-                null,
-                price2
-            )
-        )
-    ),
+    <ul>
+        <li>Apples</li>
+        <li>Oranges</li>
+    </ul>,
     document.getElementById('root')
 );
