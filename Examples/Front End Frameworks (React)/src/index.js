f930966aa2b81e1623b185c02e9db7866a27eea7
Steve Bishop <xipooo@gmail.com>
2017-12-20 12:50:19 -0800

    CreateElement with Child Elements demo

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
+
+const price1 = React.createElement('li', null, 'Price 1');
+const price2 = React.createElement('li', null, 'Price 2');
 
 ReactDOM.render(
     React.createElement(
-        'div',
+        'ul',
         null,
-        React.createElement('h1', null, 'Hello world!'), 
-        React.createElement('h3', null, 'Brought To You By The Dude')),
+        React.createElement(
+            'li',
+            null,
+            'Item 1',
+            React.createElement(
+                'ul',
+                null,
+                price1)),
+        React.createElement(
+            'li',
+            null,
+            'Item 2',
+            React.createElement(
+                'ul',
+                null,
+                price2
+            )
+        )
+    ),
     document.getElementById('root')
 );
