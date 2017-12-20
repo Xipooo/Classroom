7bead891f4e75cc34687e1e4b2c487860beae461
Steve Bishop <xipooo@gmail.com>
2017-12-20 13:02:45 -0800

    JSX Interpolation

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,10 +1,13 @@
 import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
 
+const item1 = "Apples";
+const item2 = "Oranges";
+
 ReactDOM.render(
     <ul>
-        <li>Apples</li>
-        <li>Oranges</li>
+        <li>{item1}</li>
+        <li>{item2}</li>
     </ul>,
     document.getElementById('root')
 );
