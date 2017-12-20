116395d7af88babae38c9d0321ea9554895f695f
Steve Bishop <xipooo@gmail.com>
2017-12-20 10:44:57 -0800

    ES2015 Class example

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,9 +1,10 @@
-function Dog(breed){
-    this.breed = breed;
-}
-
-Dog.prototype.rollOver = function(times){
-    console.log(`The ${this.breed} rolled over ${times} times.`);
+class Dog {
+    constructor(breed) {
+        this.breed = breed;
+    }
+    rollOver(times){
+        console.log(`The ${this.breed} rolled over ${times} times.`);
+    }
 }
 
 const sparky = new Dog("Dachshund");
