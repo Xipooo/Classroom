a0591132f3485454c3e3cdd7787a1d6831f123cb
Steve Bishop <xipooo@gmail.com>
2017-12-20 10:24:33 -0800

    Arrow Function Demo

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,1 +1,16 @@
-alert("it's alive");
+const Example = {
+    x: 81,
+    add: function() {
+        return this.x + 1;
+    }
+}
+
+const traditionalResult = Example.add();
+console.log(traditionalResult);
+
+const Example2 = {
+    x: 81,
+    add: (x) => x + 1
+}
+const arrowResult = Example2.add(99);
+console.log(arrowResult);
