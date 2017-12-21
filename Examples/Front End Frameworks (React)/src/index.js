98900b02f1d9244f1f8bda01b03f2b44d3bfeb97
Steve Bishop <xipooo@gmail.com>
2017-12-21 10:53:26 -0800

    Lifecycle methods and higher order components

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Description extends React.Component {
+    constructor(props) { super(props) }
+
+    render() {
+        return (<p>Your last name is {this.props.lastName } and you are {this.props.age} years old.</p>);
+    }
+}
+
 class Hello extends React.Component {
     constructor(props) {
         super(props);
+        console.log("constructor was called");
+        this.lastName = props.lastName;
+        this.age = props.age;
     }
+
+    componentDidMount() {
+        console.log("componentDidMount was called");
+    }
+
+    componentWillReceiveProps(props){
+        console.log("componentWillReceiveProps was called");
+    }
+
     render() {
+        console.log("render was called")
         return (
             <div>
                 <h1>Hello, {this.props.name}</h1>
+                <Description lastName={this.props.lastName} age={this.props.age} />
             </div>
         );
     }
 }
 
+
+
 ReactDOM.render(
     <div>
-        <Hello name='McFly' />
-        <Hello name='Tyler' />
-        <Hello name='Marshall' />
+        <Hello name='Marty' lastName="McFly" age={"Steve"} />
+        {/* <Hello name='Tyler' lastName="Fuqua" age={29} />
+        <Hello name='Marshall' lastName="Frink" age={12} /> */}
     </div>,
     document.getElementById('root')
 );
