6de208313837299d1ea090ba12115185219253f4
Steve Bishop <xipooo@gmail.com>
2017-12-22 11:13:42 -0800

    Filtered Example

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,17 +1,7 @@
-const someNumbers = [5, 9, 21, 2];
-
-const newNumbers = [...someNumbers, 17];
-
-console.log(newNumbers);
-
-const person = {
-    name: "Jess",
-    age: 42
+function isEnough(value){
+    return value >= 10;
 }
 
-const newPerson = {...person};
-console.log(newPerson);
+const filtered = [12, 5, 8, 130, 44].filter(isEnough);
 
-const tyler = {...person, name: "Tyler"}
-console.log(newPerson);
-console.log(tyler);
+console.log(filtered);
