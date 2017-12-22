52854d818712e3438b8a791a0d0b932ea8118a4b
Steve Bishop <xipooo@gmail.com>
2017-12-22 10:24:04 -0800

    Destructure demo

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,57 +1,20 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, createElement } from 'react';
+import { render } from 'react-dom';
 
-const Hello = (props) =>
-    <div>
-        <h1>Hello, {props.name}</h1>
-    </div>
-
-class Balance extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return (nextProps.accountBalance !== this.props.accountBalance);
-    }
-
-    render() {
-        console.log("Rendering");
-        return <h3>Savings Account Balance: ${this.props.accountBalance}</h3>
-    }
-}
-class BankAccount extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            accountBalance: 25.00,
-            incrementValue: 0
-        };
-    }
-
-    increment() {
-        // this.state.accountBalance += parseInt(this.state.incrementValue + (this.state.accountBalance * .96));
-        this.setState(
-            { accountBalance: this.state.accountBalance + parseInt(this.state.incrementValue) });
-    }
-
-    updateAmount(event) {
-        this.setState({ incrementValue: parseInt(event.target.value) });
-    }
-
-
+class Hello extends Component {
     render() {
         return (
-            <div>
-                <Balance accountBalance={this.state.accountBalance} />
-                <input type="number" onChange={this.updateAmount.bind(this)} />
-                <button onClick={this.increment.bind(this)}>Increase Amount</button>
-            </div>
-        );
+            createElement('h1', null, "Hello world!")
+        )
     }
 }
 
-ReactDOM.render(
-    <BankAccount />,
-    document.getElementById('root')
-);
+render(<Hello />, document.getElementById('root'));
+
+
+// const obj = { first: 'Foo', last: 'Bar'};
+
+// // const first = obj.first;
+// // const last = obj.last;
+
+// const { last } = obj;
