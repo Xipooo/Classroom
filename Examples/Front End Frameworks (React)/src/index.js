eb466e5f090ac74e06aefdca84b0176e572d4150
Steve Bishop <xipooo@gmail.com>
2017-12-22 10:37:24 -0800

    Spread operator demo 1

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,4 +1,11 @@
-const array = ['a', 'b'];
+const parameters = [4, 8, 2, 1, 6];
 
-const [x, y] = array;
+const maxWithoutSpread = 
+    Math.max(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4]);
+
+console.log(maxWithoutSpread);
+
+const maxUsingSpread = Math.max(...parameters);
+
+console.log(maxUsingSpread);
 
