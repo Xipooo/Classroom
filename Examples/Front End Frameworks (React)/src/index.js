81e5fc48ce2d85e9c8212a9a5f90a32cd0cb2f23
Steve Bishop <xipooo@gmail.com>
2017-12-27 11:05:50 -0800

    Final Redux Solution

--- Examples/Front End Frameworks (React)/src/index.js
+++ Examples/Front End Frameworks (React)/src/index.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
 import Counter from './Counter.js';
 
 const initialState = {
     count: 0
 }
 
-const reducer = (state = initialState, action) =>
-{
-    switch(action.type)
-    {
+const mapStateToProps = (state) => { return { count: state.count  } }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onDecrement: () => dispatch({ type: 'DECREMENT' }),
+        onIncrement: () => dispatch({ type: 'INCREMENT' }),
+        onReset: () => dispatch({ type: 'RESET' })
+    }
+}
+const ConnectedCounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
         case 'RESET':
-            return {...state, count: 0};
+            return { ...state, count: 0 };
         case 'INCREMENT':
-            return {...state, count: state.count + 1}
+            return { ...state, count: state.count + 1 };
         case 'DECREMENT':
-            return {...state, count: state.count - 1};
+            return { ...state, count: state.count - 1 };
         default:
             return state;
     }
@@ -24,35 +33,9 @@ const reducer = (state = initialState, action) =>
 
 const store = createStore(reducer);
 
-
-const renderApp = () =>
-{
-    ReactDOM.render(
-        <Counter 
-            count={ store.getState().count }
-            onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-            onReset={() => store.dispatch({type: 'RESET'})}
-            onDecrement={() => store.dispatch({type: 'DECREMENT'})}
-        />,
-        document.getElementById('root')
-    )
-}
-store.subscribe(
-    () => {
-        console.log("State Value: ", store.getState());
-        renderApp();
-    }
-);
-
-renderApp();
-
-// console.log('State at start: ', store.getState());
-
-// store.dispatch({type: 'INCREMENT' });
-// store.dispatch({type: 'INCREMENT' });
-// store.dispatch({type: 'INCREMENT' });
-// store.dispatch({type: 'DECREMENT' });
-// store.dispatch({type: 'RESET'});
-// store.dispatch({type: 'INCREMENT' });
-// store.dispatch({type: 'INCREMENT' });
-// store.dispatch({type: 'DECREMENT' });
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedCounter />
+    </Provider>,
+    document.getElementById('root')
+)
