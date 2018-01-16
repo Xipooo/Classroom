c84981948769c363777f10aac6a859a6a23cec8d
Steve Bishop <xipooo@gmail.com>
2018-01-16 14:20:01 -0800

    Debugging Demo

---
+++ Examples/Programming Foundations (Java)/Debugging/src/com/codercamps/redmond/debugging/Startup.java
@@ -0,0 +1,17 @@
+package com.codercamps.redmond.debugging;
+
+public class Startup {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String message = "My Message";
+		
+		System.out.println("First");
+		System.out.println(message);
+		System.out.println("Third");
+		
+		message = "My Other Message";
+		System.out.println(message);
+	}
+
+}
