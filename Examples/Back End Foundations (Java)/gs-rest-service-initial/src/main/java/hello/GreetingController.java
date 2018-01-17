48aae19475b1eddb6ed12030d4a1c3d79d7b0d64
Steve Bishop <xipooo@gmail.com>
2018-01-17 11:01:35 -0800

    MVC Resful Services Demo

---
+++ Examples/Back End Foundations (Java)/gs-rest-service-initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+	private static final String template = "Welcome, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name", defaultValue="User") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+}
